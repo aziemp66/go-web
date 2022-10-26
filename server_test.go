9a39252494913e7bd0b5e9863c594965e805e4f2
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-26 20:44:05 +0700

    Server

---
+++ server_test.go
@@ -0,0 +1,17 @@
+package goweb
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	server := http.Server{
+		Addr: "localhost:5000",
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
