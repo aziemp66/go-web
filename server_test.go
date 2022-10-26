2d3bd0871701fdf7368560c9029fe1b95dc2af06
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-26 21:32:06 +0700

    Request

--- server_test.go
+++ server_test.go
@@ -21,3 +21,49 @@ func TestServer(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World!")
+	})
+
+	mux.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hi World!")
+	})
+
+	mux.HandleFunc("/images/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Images")
+	})
+
+	mux.HandleFunc("/images/thumbnail", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "thumbnail")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:5000",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestRequest(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, r.Method)
+		fmt.Fprintln(w, r.RequestURI)
+	}
+
+	server := http.Server{
+		Addr:    "localhost:5000",
+		Handler: handler,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
