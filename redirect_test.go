04a67c4a9434ba6d9dd0f59086a7258323505e25
Azie Melza Pratama <aziemp66@gmail.com>
2022-11-05 19:11:59 +0700

    Redirecting

---
+++ redirect_test.go
@@ -0,0 +1,28 @@
+package goweb
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func RedirectTo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello Redirect")
+}
+
+func RedirectFrom(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/redirect-to", http.StatusTemporaryRedirect)
+}
+
+func TestRedirect(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/redirect-from", RedirectFrom)
+	mux.HandleFunc("/redirect-to", RedirectTo)
+
+	server := http.Server{
+		Addr:    ":3000",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
+}
