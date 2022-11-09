2e5ad34d30a48a8be2c6c79ec97c93bd43f95269
Azie Melza Pratama <aziemp66@gmail.com>
2022-11-09 19:20:14 +0700

    Error Handler Middleware

--- middleware_test.go
+++ middleware_test.go
@@ -35,3 +35,40 @@ func TestMiddleware(t *testing.T) {
 		panic(err)
 	}
 }
+
+type ErrorHandler struct {
+	Handler http.Handler
+}
+
+func (handler *ErrorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		err := recover()
+		fmt.Println("RECOVER :", err)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Error : %s", err)
+		}
+	}()
+
+	handler.Handler.ServeHTTP(w, r)
+}
+
+func TestErrorMiddleware(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		panic("Panic Gwe Guys")
+	})
+
+	errorHandler := &ErrorHandler{Handler: &LogMiddleware{Handler: mux}}
+
+	server := http.Server{
+		Addr:    ":3000",
+		Handler: errorHandler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+
+}
